00c77cb0e262fb89012e66a5a40e416dea85e6e2
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-02-23 11:37:57 -0600

    simple geocoder task

--- src/tasks/geocode.py
+++ src/tasks/geocode.py
@@ -5,7 +5,34 @@ transformed/augmented structure so that downstream tasks can make further
 modifications or update the document in the datastore. 
 
 """
+
+import requests
+import json
+from config import settings
+
 def run(data):
-    print "in example"
-    data["hiphophoray"] = "ho, hey, ho"
+    if 'formattedAddress' not in data['geo']['addressComponents']:
+        return data
+
+    url = 'http://dev.virtualearth.net/REST/v1/Locations/%s?key=%s' % (
+        data['geo']['addressComponents']['formattedAddress'], settings.MAP_KEY) 
+    
+    r = requests.get(url)
+    
+    if r.status_code != 200:
+        return data
+
+    json_data = json.loads(r.text)
+
+    if len(json_data['resourceSets']) == 0:
+        return data
+
+    if len(json_data['resourceSets'][0]['resources']) == 0:
+        return data
+
+    coords = json_data['resourceSets'][0]['resources'][0]['point']['coordinates']
+    
+    # API gives us lat,lng and we need lng,lat
+    data['geo']['coordinates'] = coords[::-1]
+    
     return data
