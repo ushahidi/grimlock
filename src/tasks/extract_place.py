b53579df1e4e16a75d8303acdc309a678c3f81ad
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-03-17 16:51:54 -0500

    updating tags array to nested documents

---
+++ src/tasks/extract_place.py
@@ -0,0 +1,4 @@
+# nltk to extract known entities
+# fuzzy search for LOC entities against known city/region/country https://github.com/seatgeek/fuzzywuzzy
+# build not-a-city-etc, city, region, country
+# set properties on document
---
+++ src/tasks/relevance_classifier.py
@@ -0,0 +1,1 @@
+# topical analysis - danger, 
