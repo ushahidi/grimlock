24cf9c214dac66521a77c9d244efe0bf48e6febc
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-03-31 22:04:51 -0500

    removing configs

--- src/config/staging_settings.py
+++
@@ -1,7 +0,0 @@
-MAP_KEY = 'AgCXLA2g3bL4oehlmM1wCDEUJpbojq2TwQSqB7vojBvIe3TIT3GERRUHSw1Yqob7'
-MONGO_HOST = "mongodb://cnstaging:yLg14C2XZ86Pn3x@oceanic.mongohq.com:10010/cnstaging"
-MONGO_PORT = 10021
-MONGO_DB = 'cnstaging'
-REDIS_HOST = 'beardfish.redistogo.com'
-REDIS_PORT = 9437
-REDIS_PASSWORD = '34db73a463158e22aa2099e1050e8acd'
